41377ec355ce4d75e2b14574e4a7d3884603e522
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2010-08-27 15:21:44 +0530

    submit comments using ajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,10 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create!(params[:comment])
-    redirect_to @post
+
+    respond_to do |format|
+      format.html { redirect_to @post }
+      format.js
+    end
   end
 end
