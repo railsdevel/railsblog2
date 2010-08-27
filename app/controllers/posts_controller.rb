8a4d66c99443772da2127808ac24b34e00bd7de4
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2010-08-27 14:51:54 +0530

    added respond_to json to index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,7 @@ class PostsController < ApplicationController
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @posts }
+      format.json { render :json => @posts }
     end
   end
 
